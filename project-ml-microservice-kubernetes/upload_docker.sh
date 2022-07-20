fe9abd514a27305516f41a29436b1e902e1f533e
werneroscar <wernertsekumah@gmail.com>
2022-07-20 11:29:34 +0000

    new comit

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=werneroscar/udacity-project-4
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker tag app werneroscar/udacity-project-4:latest
 # Step 3:
 # Push image to a docker repository
+echo "pushing to dockerhub...."
+docker push werneroscar/udacity-project-4:latest
