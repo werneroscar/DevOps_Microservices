c7346853bf02a28250ad035e67ada64759a216b1
werner-oscar <werner.tsekumah@amalitech.org>
2022-07-21 10:05:07 +0000

    new changes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -7,10 +7,9 @@
 # Create dockerpath
 # dockerpath=<your docker ID/path>
 dockerpath=werneroscar/udacity-project-4
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
 docker tag app werneroscar/udacity-project-4:latest
 # Step 3:
 # Push image to a docker repository
